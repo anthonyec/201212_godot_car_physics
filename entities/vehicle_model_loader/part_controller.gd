8b2e6be4c9096d10af33294b5c74c7d7d6ccbf35
Anthony Cossins <anthonyecossins@gmail.com>
2020-12-31 00:06:10 +0000

    Remove old func and add helper method for sides

--- entities/vehicle_model_loader/part_controller.gd
+++ entities/vehicle_model_loader/part_controller.gd
@@ -1,14 +1,7 @@
 extends Node
 
-# https://godotengine.org/qa/45609/how-do-you-rotate-spatial-node-around-axis-given-point-space
-func rotate_around_old(obj, point, axis, angle):
-	var rot = angle + obj.rotation.y 
-	var tStart = point
-	obj.global_translate(-tStart)
-	obj.transform = obj.transform.rotated(axis, -rot)
-	obj.global_translate(tStart)
-	
 # https://godotengine.org/qa/52262/is-there-any-equivalent-in-gdscript-the-rotatearound-method
+# Note: if you want it to work in global space regardless of parenting, use global_transform instead of transform.
 func rotate_around(node, point, axis, angle):
 	# Get transform
 	var trans = node.global_transform
@@ -22,16 +15,19 @@ func rotate_around(node, point, axis, angle):
 	# Set the result back
 	node.global_transform = Transform(rotated_basis, rotated_origin)
 
-func open_door():
-	pass
-	
-func close_door():
-	pass
-	
+func is_left_side(name: String) -> bool:
+	return name.to_lower().find("left*") != -1
 
+func open_door(name: String):
+	var angle = -30 if is_left_side(name) else 30
+	rotate_part(name, angle)
+	
+func close_door(name: String):
+	rotate_part(name, 0)
+	
 var original_transforms: Dictionary = {}
 
-func cache_original_transform(node: Spatial):
+func cache_and_restore_original_transform(node: Spatial):
 	if !original_transforms.has(node.name):
 		original_transforms[node.name] = node.transform
 	else:
@@ -41,19 +37,22 @@ func rotate_part(name: String, angle: float):
 	var radians = deg2rad(angle)
 	var part: MeshInstance = get_parent().get_node("Model").get_node(name)
 	
+	# Check part exists, it may have been detached!
 	if !part:
 		return null
 	
 	var pivot: Spatial = get_parent().get_node("Model").get_node("$_HINGE_" + name)
 	var pivot_point: Vector3 = pivot.global_transform.origin
-	var distance_from_pivot: float = part.global_transform.origin.distance_to(pivot_point)
 	
-	cache_original_transform(part)
+	# Keep the original transformation and apply it before rotating.
+	# Otherwise when rotating, it will add degrees to previous rotation, causing
+	# it to spin around
+	cache_and_restore_original_transform(part)
 		
 	rotate_around(
 		part,
 		pivot_point,
-		pivot.global_transform.basis.y,
+		pivot.global_transform.basis.y, # Local up direction of pivot point
 		radians
 	)
 	
